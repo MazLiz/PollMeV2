c0fac8925caccc3efda2cc707d5e7a328e8c8912
Lisa <lisetta@lisetta-PC>
2013-12-28 19:11:18 +0100

    il commit della morteeee! XD

--- GoogleTutorial2/gen/com/example/googletutorial2/R.java
+++ GoogleTutorial2/gen/com/example/googletutorial2/R.java
@@ -21,9 +21,11 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_search=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int action_search=0x7f08000b;
         public static final int action_settings=0x7f08000a;
         public static final int btnIntend=0x7f080006;
         public static final int btnIntendS=0x7f080007;
@@ -50,6 +52,7 @@ public final class R {
         public static final int photo=0x7f070004;
     }
     public static final class string {
+        public static final int action_search=0x7f05000f;
         public static final int action_settings=0x7f050001;
         public static final int album_name=0x7f050007;
         public static final int app_name=0x7f050000;
